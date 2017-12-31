9474cc7537a199453874032cd5a9bcdadecc1590
Alexey Korolev <korolev2009@gmail.com>
2017-12-31 14:17:38 +0200

    Running a service in the foreground

--- app/src/main/java/net/alexandroid/servicepermisssionbroadcastfirststeps/DownloadService.java
+++ app/src/main/java/net/alexandroid/servicepermisssionbroadcastfirststeps/DownloadService.java
@@ -1,15 +1,20 @@
 package net.alexandroid.servicepermisssionbroadcastfirststeps;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class DownloadService extends Service {
 
     public static final String URL = "URL";
+    public static final int ONGOING_NOTIFICATION_ID = 987;
+    public static final String CHANNEL_DEFAULT_IMPORTANCE = "Channel";
 
     public static void startService(Activity activity, String url) {
         Intent intent = new Intent(activity, DownloadService.class);
@@ -21,10 +26,22 @@ public class DownloadService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("TAG", "DownloadService # onStartCommand");
 
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_DEFAULT_IMPORTANCE)
+                .setContentTitle(getText(R.string.notification_title))
+                .setContentText(getText(R.string.notification_message))
+                .setSmallIcon(R.drawable.ic_stat_download)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(ONGOING_NOTIFICATION_ID, notification);
+
         String url = intent.getStringExtra(URL);
         Log.d("TAG", "DownloadService # URL: " + url);
 
-        stopSelf();
+        //stopSelf();
         return START_STICKY;
     }
 
