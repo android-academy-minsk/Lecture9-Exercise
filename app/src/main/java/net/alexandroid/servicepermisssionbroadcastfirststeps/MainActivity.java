ca0d4d8db0080d63610b742566a80f721c117caf
Alexey Korolev <korolev2009@gmail.com>
2017-12-31 13:17:25 +0200

    Check is permission granted and request permission

--- app/src/main/java/net/alexandroid/servicepermisssionbroadcastfirststeps/MainActivity.java
+++ app/src/main/java/net/alexandroid/servicepermisssionbroadcastfirststeps/MainActivity.java
@@ -1,52 +1,66 @@
 package net.alexandroid.servicepermisssionbroadcastfirststeps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    public static final int PERMISSIONS_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setFab();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    private void setFab() {
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onFabClick();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void onFabClick() {
+        if (isPermissionGranted()) {
+            startDownloadService();
+        } else {
+            requestPermission();
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private boolean isPermissionGranted() {
+        return ContextCompat.checkSelfPermission(this, PERMISSION) == PackageManager.PERMISSION_GRANTED;
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void startDownloadService() {
+        Log.d("TAG", "startDownloadService");
+    }
+
+    private void requestPermission() {
+        // Should we show an explanation?
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, PERMISSION)) {
+            // Show an explanation to the user.
+            // After the user sees the explanation, try again to request the permission.
+        } else {
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(this, new String[]{PERMISSION}, PERMISSIONS_REQUEST_CODE);
+            // PERMISSIONS_REQUEST_CODE is an app-defined int constant.
+            // The callback method gets the result of the request.
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
