f5290565d60b96d245d7969a95632f161285fcb8
Alexey Korolev <korolev2009@gmail.com>
2018-01-01 12:40:22 +0200

    Create notification channel to support API 26+

--- app/src/main/java/net/alexandroid/servicepermisssionbroadcastfirststeps/DownloadService.java
+++ app/src/main/java/net/alexandroid/servicepermisssionbroadcastfirststeps/DownloadService.java
@@ -2,9 +2,13 @@ package net.alexandroid.servicepermisssionbroadcastfirststeps;
 
 import android.app.Activity;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
@@ -25,9 +29,20 @@ public class DownloadService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("TAG", "DownloadService # onStartCommand");
+        startForeground();
 
+        String url = intent.getStringExtra(URL);
+        Log.d("TAG", "DownloadService # URL: " + url);
+
+        //stopSelf();
+        return START_STICKY;
+    }
+
+    private void startForeground() {
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        createNotificationChannel();
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_DEFAULT_IMPORTANCE)
                 .setContentTitle(getText(R.string.notification_title))
@@ -37,12 +52,32 @@ public class DownloadService extends Service {
                 .build();
 
         startForeground(ONGOING_NOTIFICATION_ID, notification);
+    }
 
-        String url = intent.getStringExtra(URL);
-        Log.d("TAG", "DownloadService # URL: " + url);
+    private void createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            // The user-visible name of the channel.
+            CharSequence name = getString(R.string.channel_name);
+            // The user-visible description of the channel.
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_DEFAULT_IMPORTANCE, name, importance);
 
-        //stopSelf();
-        return START_STICKY;
+            // Configure the notification channel.
+            mChannel.setDescription(description);
+            mChannel.enableLights(true);
+
+            // Sets the notification light color for notifications posted to this
+            // channel, if the device supports this feature.
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(mChannel);
+            }
+        }
     }
 
     @Override
