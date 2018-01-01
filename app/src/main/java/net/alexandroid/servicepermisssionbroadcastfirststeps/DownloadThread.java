27df1c330f8d9502ba724b0096adeefb31f95f03
Alexey Korolev <korolev2009@gmail.com>
2018-01-01 14:31:18 +0200

    Download file

--- app/src/main/java/net/alexandroid/servicepermisssionbroadcastfirststeps/DownloadThread.java
+++ app/src/main/java/net/alexandroid/servicepermisssionbroadcastfirststeps/DownloadThread.java
@@ -4,7 +4,14 @@ import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -26,7 +33,67 @@ public class DownloadThread extends Thread {
         File file = createFile();
         if (file == null) {
             mDownloadCallBack.onError("Can't create file");
-            //return;
+            return;
+        }
+
+        HttpURLConnection connection = null;
+        InputStream inputStream = null;
+        FileOutputStream fos = null;
+
+        try {
+            URL url = new URL(mUrl);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                mDownloadCallBack.onError("Server returned HTTP response code: "
+                        + connection.getResponseCode() + " - " + connection.getResponseMessage());
+            }
+            int fileLength = connection.getContentLength();
+            Log.d("TAG", "File size: " + fileLength / 1024 + " KB");
+
+            // Input stream (Downloading file)
+            inputStream = new BufferedInputStream(url.openStream(), 8192);
+
+            // Output stream (Saving file)
+            fos = new FileOutputStream(file.getPath());
+
+            int progress = 0;
+            int next;
+            byte[] data = new byte[1024];
+            while ((next = inputStream.read(data)) != -1) {
+                fos.write(data, 0, next);
+
+                int count = ((int) fos.getChannel().size()) * 100 / fileLength;
+                if (count > progress) {
+                    progress = count;
+                    mDownloadCallBack.onProgressUpdate(progress);
+                }
+            }
+
+            mDownloadCallBack.onDownloadFinished(file.getPath());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            mDownloadCallBack.onError("MalformedURLException: " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+            mDownloadCallBack.onError("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.flush();
+                    fos.close();
+                }
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
     }
 
