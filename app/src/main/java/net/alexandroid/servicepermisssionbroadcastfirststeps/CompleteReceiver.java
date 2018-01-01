9b6179943d069c267c1ecad67268c6a91b11b0bf
Alexey Korolev <korolev2009@gmail.com>
2018-01-01 15:32:24 +0200

    Add BroadcastReceiver (CompleteReceiver)

---
+++ app/src/main/java/net/alexandroid/servicepermisssionbroadcastfirststeps/CompleteReceiver.java
@@ -0,0 +1,17 @@
+package net.alexandroid.servicepermisssionbroadcastfirststeps;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class CompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("TAG", "CompleteReceiver # onReceive");
+
+        Toast.makeText(context, "File downloaded", Toast.LENGTH_LONG).show();
+    }
+}
